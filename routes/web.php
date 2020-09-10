301a911dc79ecf48529464c60b4fea9276204476
David S. García (Azæl) <azael@dragonspark.tech>
2020-09-10 17:58:18 -0500

    chore: update routes to allow access to the test controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SFTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test/elements', [SFTestController::class, 'lookup_frontend_modules']);
+Route::get('/test/elements/{module}', [SFTestController::class, 'lookup_specific_module']);
