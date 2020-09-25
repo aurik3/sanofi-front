6f03abe3ad53a8b052a9200e6c783ebe94bae1d0
David S. García (Azæl) <azael@dragonspark.tech>
2020-09-25 13:38:02 -0500

    fix: correct mistakes in logout styling

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,11 +5,24 @@
     @if(empty($__env->yieldContent('hide_login')))
     <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-            <a href="#" class="btn btn-login">
+            <a href="{{ route('login') }}" class="btn btn-login">
                 <i class="far fa-user-circle"></i>
                 Ingreso
             </a>
         </li>
     </ul>
+    @endif
+    @if(empty($__env->yieldContent('hide_logout')))
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <form action="{{ route('m-logout') }}" method="POST">
+                    {{ csrf_field() }}
+                    <button class="btn btn-login" type="submit">
+                        <i class="far fa-key"></i>
+                        Salir
+                    </button>
+                </form>
+            </li>
+        </ul>
     @endif
 </nav>
